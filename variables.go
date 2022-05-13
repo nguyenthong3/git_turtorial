78f51827a4894994ecdbb45ec78b8fc4e7ffbf64
Nguyen Dinh Duy Thong <nguyendinhduythong@gmail.com>
2022-05-13 13:54:20 +0700

    first commit about Golang Web Development

---
+++ variables.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+var (
+	actorName string = "X Y"
+	companion string = "Z"
+	number    int    = 45
+)
+
+func e() {
+	fmt.Println(actorName, companion, number)
+}
